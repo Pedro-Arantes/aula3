f98b1d0607ebac4686cf0af209b40b64520d75ce
Pedro Vianna <pedroviannabiomed@gmail.com>
2021-11-29 18:58:33 -0300

    :pencil: versao 0.0.1

---
+++ comentarios.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "<pre>
+O comentário dentro do php pode ser feito com aspas duplas barras ou asterisco barra
+<h1>Autor:Pedro Arantes</h1>
+</pre>";
+//
+/* */
+# tambem pode hashtag
+//<pre> tb formata o texto permitindo espaços sem utilizar <br> alem de anular comentarios dentro dele.
