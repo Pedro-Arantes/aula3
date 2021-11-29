3eb4a9e35c6fd2779f0aaa8355ff0293ad198b2c
Pedro Vianna <pedroviannabiomed@gmail.com>
2021-11-29 19:13:06 -0300

    versao 0.0.2

---
+++ formulario.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario</title>
+</head>
+<body>
+    <!--  tags sao os comandos em html-->
+    <h1>Meu Primeiro Formulário</h1>
+    <hr>
+
+    <form action="#">
+        Nome: <input type="text"  required><br> <br>       Idade: <input type="number"><br><br>
+        Data de inscrição: <input type="date"><br><br>
+        E-mail: <input type="email" required><br><br>
+        Curso
+        <select name="" id="">
+            <option value="">Excel</option>
+            <option value="">Programador web</option>
+            <option value="">Banco de dados</option>
+        </select><br><br>
+        <input type="submit" value= "Cadastrar funcionario">
+
+
+    </form>
+</body>
+</html>
